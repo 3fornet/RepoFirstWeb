fda6f0001e02c9a59b25f17d71489c4b84d5f023
operator <operator@OPERATOR-PC>
2017-03-16 16:23:21 +0100

    Agregar archivos de proyecto.

---
+++ RepoFirstWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RepoFirstWeb.MvcApplication" Language="C#" %>
---
+++ RepoFirstWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RepoFirstWeb.Startup))]
+namespace RepoFirstWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
